b17a1919fe15ec27a535b4bc78e6a8f18e48e912
Sara Hossny <104026246+Sara-Hossny@users.noreply.github.com>
2022-12-16 14:31:54 +0200

    json_edit
    handle xml tags with attribute

--- Library/XMLTree.py
+++ Library/XMLTree.py
@@ -201,9 +201,10 @@ class Tree:
         sep = level * "\t"
         flag_list_close=False
         flag_set_close=False
+        length=len(root.children)
         if not root:
             return
-        if(len(root.children)==0):
+        if(length==0):
             if(len(temp)==1 and root.tag_name==temp[0] ):
                 temp.pop()
                 print(sep+" ]")
@@ -223,26 +224,32 @@ class Tree:
                 if(flag_set_close):
                     print(sep+" }")
                     flag_set_close=False     
-        elif(len(root.children)==1 ):
+        elif(length==1 ):
             if(root.children[0].value!=None):
-                print(sep+"\""+root.tag_name+"\":\""+root.children[0].value+"\",")
-        elif(len(root.children)>2):
+                    print(sep+"\""+root.tag_name +"\":\"",end=" ")
+                    if root.has_attribute:
+                        print("{\""+root.attribute_name+"\":"+root.attribute_value+"}")
+                    else:
+                        print(root.children[0].value+"\",")
+        elif(length>2):
             if(level==0):
                 print("{\""+root.tag_name+"\"[")
                 temp.append(root.tag_name)
             elif(root.children[0].tag_name!=root.children[2].tag_name):
-                print(sep+"\""+root.tag_name,end=" ")
-                if root.has_attribute:
-                    print(f'{root.attribute_name}={root.attribute_value}')
-                else:
-                    print("\":{")
-                temp1.append(root.tag_name)
+                    print(sep+"\""+root.tag_name +"\":\"",end=" ")
+                    if root.has_attribute:
+                        print("{\""+root.attribute_name+"\":"+root.attribute_value+"}{")
+                    else:
+                        print(root.children[0].value+"\",")
+                        print("\":{")
+                    temp1.append(root.tag_name)
             else:   
                 if root.is_open_tag:
-                    print(sep+"\""+root.tag_name,end=" ")
+                    print(sep+"\""+root.tag_name +"\":\"",end=" ")
                     if root.has_attribute:
-                        print(f'{root.attribute_name}={root.attribute_value}')
+                        print("{\""+root.attribute_name+"\":"+root.attribute_value+"}{")
                     else:
+                        print(root.children[0].value+"\",")
                         print("\":[")
                         temp.append(root.tag_name)
                     if root.hasValue and root.children:
@@ -334,77 +341,127 @@ class Tree:
 
 if __name__ == '__main__':
     file_string = """
-<users>
-<usr/><user/><!--    --><easy/><user ><id >1<name>Ahmed Ali
-</name>
-        <posts><post><body>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</body><topics><topic>economy</topic><topic>finance</topic></topics>
-            </post>
-            <post>
-                <body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </body>
-                <topics>
-                    <topic>
-                        solar_energy
-                    </topic>
-                </topics>
-            </post>
-        </posts>
-        <followers>
-            <follower>
-                <id>2</id>
-            
-            <follower>
-                <id>3</id>
-           
-        </followers>
-    </user>
-    <user>
-        <id>2</id>
-        <name>Yasser Ahmed</name>
-        <posts>
-            <post>
-                <body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </body>
-                <topics>
-                    <topic>
-                        education
-                    </topic>
-                </topics>
-            </post>
-        </posts>
-        <followers>
-            <follower>
-                <id>1</id>
-            </follower>
-        </followers>
-    </user>
-    <user>
-        <id>3</id>
-        <name>Mohamed Sherif</name>
-        <posts>
-            <post>
-                <body>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                </body>
-                <topics>
-                    <topic>
-                        sports
-                    </topic>
-                </topics>
-            </post>
-        </posts>
-        
-            <follower>
-                <id>1</a>
-            
-        </followers>
-
-
+<?xml version="1.0"?>
+<catalog>
+   <book id="bk101">
+      <author>Gambardella, Matthew</author>
+      <title>XML Developer's Guide</title>
+      <genre>Computer</genre>
+      <price>44.95</price>
+      <publish_date>2000-10-01</publish_date>
+      <description>An in-depth look at creating applications 
+      with XML.</description>
+   </book>
+   <book id="bk102">
+      <author>Ralls, Kim</author>
+      <title>Midnight Rain</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2000-12-16</publish_date>
+      <description>A former architect battles corporate zombies, 
+      an evil sorceress, and her own childhood to become queen 
+      of the world.</description>
+   </book>
+   <book id="bk103">
+      <author>Corets, Eva</author>
+      <title>Maeve Ascendant</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2000-11-17</publish_date>
+      <description>After the collapse of a nanotechnology 
+      society in England, the young survivors lay the 
+      foundation for a new society.</description>
+   </book>
+   <book id="bk104">
+      <author>Corets, Eva</author>
+      <title>Oberon's Legacy</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2001-03-10</publish_date>
+      <description>In post-apocalypse England, the mysterious 
+      agent known only as Oberon helps to create a new life 
+      for the inhabitants of London. Sequel to Maeve 
+      Ascendant.</description>
+   </book>
+   <book id="bk105">
+      <author>Corets, Eva</author>
+      <title>The Sundered Grail</title>
+      <genre>Fantasy</genre>
+      <price>5.95</price>
+      <publish_date>2001-09-10</publish_date>
+      <description>The two daughters of Maeve, half-sisters, 
+      battle one another for control of England. Sequel to 
+      Oberon's Legacy.</description>
+   </book>
+   <book id="bk106">
+      <author>Randall, Cynthia</author>
+      <title>Lover Birds</title>
+      <genre>Romance</genre>
+      <price>4.95</price>
+      <publish_date>2000-09-02</publish_date>
+      <description>When Carla meets Paul at an ornithology 
+      conference, tempers fly as feathers get ruffled.</description>
+   </book>
+   <book id="bk107">
+      <author>Thurman, Paula</author>
+      <title>Splish Splash</title>
+      <genre>Romance</genre>
+      <price>4.95</price>
+      <publish_date>2000-11-02</publish_date>
+      <description>A deep sea diver finds true love twenty 
+      thousand leagues beneath the sea.</description>
+   </book>
+   <book id="bk108">
+      <author>Knorr, Stefan</author>
+      <title>Creepy Crawlies</title>
+      <genre>Horror</genre>
+      <price>4.95</price>
+      <publish_date>2000-12-06</publish_date>
+      <description>An anthology of horror stories about roaches,
+      centipedes, scorpions  and other insects.</description>
+   </book>
+   <book id="bk109">
+      <author>Kress, Peter</author>
+      <title>Paradox Lost</title>
+      <genre>Science Fiction</genre>
+      <price>6.95</price>
+      <publish_date>2000-11-02</publish_date>
+      <description>After an inadvertant trip through a Heisenberg
+      Uncertainty Device, James Salway discovers the problems 
+      of being quantum.</description>
+   </book>
+   <book id="bk110">
+      <author>O'Brien, Tim</author>
+      <title>Microsoft .NET: The Programming Bible</title>
+      <genre>Computer</genre>
+      <price>36.95</price>
+      <publish_date>2000-12-09</publish_date>
+      <description>Microsoft's .NET initiative is explored in 
+      detail in this deep programmer's reference.</description>
+   </book>
+   <book id="bk111">
+      <author>O'Brien, Tim</author>
+      <title>MSXML3: A Comprehensive Guide</title>
+      <genre>Computer</genre>
+      <price>36.95</price>
+      <publish_date>2000-12-01</publish_date>
+      <description>The Microsoft MSXML3 parser is covered in 
+      detail, with attention to XML DOM interfaces, XSLT processing, 
+      SAX and more.</description>
+   </book>
+   <book id="bk112">
+      <author>Galos, Mike</author>
+      <title>Visual Studio 7: A Comprehensive Guide</title>
+      <genre>Computer</genre>
+      <price>49.95</price>
+      <publish_date>2001-04-16</publish_date>
+      <description>Microsoft Visual Studio 7 is explored in depth,
+      looking at how Visual Basic, Visual C++, C#, and ASP+ are 
+      integrated into a comprehensive development 
+      environment.</description>
+   </book>
+</catalog>
   """
     xmlTree = Tree()
     xmlTree.parser(file_string)
-    xmlTree.correter_XML()
-    xmlTree.visualizeXML()
-    print(len(xmlTree.created_nodes))
+    xmlTree.visualizeJSON()
