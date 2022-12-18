27bba5ba9abee4e9900b917bd0af766da2bae3bf
RanaHossny <104026566+RanaHossny@users.noreply.github.com>
2022-12-18 11:14:57 +0200

    update gui.py

--- gui/gui.py
+++ gui/gui.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import tkinter.font as tkFont
 from tkinter import *
 from tkinter import filedialog
+import tkinter.ttk as ttk
 
 
 class App:
@@ -26,74 +27,74 @@ class App:
         label = Label(self.root, image=xml_cover)
         label.place(x=0, y=0)
         Button1 = tk.Button(self.root)
-        Button1["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times', size=10)
+        Button1["bg"] = "black"
+        ft = tkFont.Font(family='Times', size=12)
         Button1["font"] = ft
-        Button1["fg"] = "#000000"
+        Button1["fg"] = "white"
         Button1["justify"] = "center"
         Button1["text"] = "check valid"
-        Button1.place(x=360, y=220, width=70, height=25)
+        Button1.place(x=360, y=220, width=85, height=25)
         Button1["command"] = self.check_valid
+        Button1["borderwidth"] = "3px"
+
 
         Button2 = tk.Button(self.root)
-        Button2["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times', size=10)
+        Button2["bg"] = "black"
+        ft = tkFont.Font(family='Times', size=12)
         Button2["font"] = ft
-        Button2["fg"] = "#000000"
+        Button2["fg"] = "white"
         Button2["justify"] = "center"
         Button2["text"] = "Fix errors"
-        Button2.place(x=360, y=250, width=70, height=25)
+        Button2.place(x=360, y=250, width=85, height=25)
         Button2["command"] = self.fix_errors
+        Button2["borderwidth"] = "3px"
+
 
         Button3 = tk.Button(self.root)
-        Button3["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times', size=10)
+        Button3["bg"] = "black"
+        ft = tkFont.Font(family='Times', size=12)
         Button3["font"] = ft
-        Button3["fg"] = "#000000"
+        Button3["fg"] = "white"
         Button3["justify"] = "center"
         Button3["text"] = "Prettify"
-        Button3.place(x=360, y=280, width=70, height=25)
+        Button3.place(x=360, y=280, width=85, height=25)
         Button3["command"] = self.prettify
+        Button3["borderwidth"] = "3px"
+
 
         Button4 = tk.Button(self.root)
-        Button4["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times', size=10)
+        Button4["bg"] = "black"
+        ft = tkFont.Font(family='Times', size=12)
         Button4["font"] = ft
-        Button4["fg"] = "#000000"
+        Button4["fg"] = "white"
         Button4["justify"] = "center"
         Button4["text"] = "Minify"
-        Button4.place(x=360, y=310, width=70, height=25)
+        Button4.place(x=360, y=310, width=85, height=25)
         Button4["command"] = self.minify
+        Button4["borderwidth"] = "3px"
 
         Button5 = tk.Button(self.root)
-        Button5["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times', size=10)
+        Button5["bg"] = "black"
+        ft = tkFont.Font(family='Times', size=12)
         Button5["font"] = ft
-        Button5["fg"] = "#000000"
+        Button5["fg"] = "white"
         Button5["justify"] = "center"
         Button5["text"] = "Compress"
-        Button5.place(x=360, y=340, width=70, height=25)
+        Button5.place(x=360, y=340, width=85, height=25)
         Button5["command"] = self.compress
+        Button5["borderwidth"] = "3px"
+
 
         Button6 = tk.Button(self.root)
-        Button6["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times', size=10)
+        Button6["bg"] = "black"
+        ft = tkFont.Font(family='Times', size=12)
         Button6["font"] = ft
-        Button6["fg"] = "#000000"
+        Button6["fg"] = "white"
         Button6["justify"] = "center"
         Button6["text"] = "To JSON"
-        Button6.place(x=360, y=370, width=70, height=25)
+        Button6.place(x=360, y=370, width=85, height=25)
         Button6["command"] = self.convert_to_json
-
-        Button7 = tk.Button(self.root)
-        Button7["bg"] = "#f0f0f0"
-        ft = tkFont.Font(family='Times', size=10)
-        Button7["font"] = ft
-        Button7["fg"] = "#000000"
-        Button7["justify"] = "center"
-        Button7["text"] = "Prettify"
-        Button7.place(x=360, y=400, width=70, height=25)
-        Button7["command"] = self.prettify
+        Button6["borderwidth"] = "3px"
 
         input_text = tk.Text(self.root)
         input_text["borderwidth"] = "3px"
@@ -101,6 +102,7 @@ class App:
         input_text["font"] = ft
         input_text["fg"] = "black"
         input_text.place(x=50, y=150, width=300, height=400)
+
 
         output_text = tk.Text(self.root, state='disabled')
         # Insert the string into the Text widget
@@ -111,10 +113,15 @@ class App:
         output_text["fg"] = "black"
         output_text.place(x=450, y=150, width=300, height=400)
 
+
+
+        
+
         welcome_label = tk.Label(self.root)
         ft = tkFont.Font(family='Arial', size=14)
         welcome_label["font"] = ft
-        welcome_label["fg"] = "#333333"
+        welcome_label["fg"] = "white"
+        welcome_label["bg"] = "black"
         welcome_label["justify"] = "center"
         welcome_label["text"] = "Welcome to XML editor"
         welcome_label.place(x=280, y=50, width=234, height=41)
