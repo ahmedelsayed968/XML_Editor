bc546127edeebf3553525a7f76ede78f0c42efc9
sewar-askar <66571028+sewar-askar@users.noreply.github.com>
2023-02-01 12:38:16 +0200

    Update Network Analysis.py

--- Library/Network Analysis.py
+++ Library/Network Analysis.py
@@ -1,70 +1,89 @@
-from Dictionary import Dictionary
 from UserData import DataBase
 from Handling_files import read_file
 
-def most_influencer(graph_of_users):
-    influencers = []
-    max_followers = 0
-    for user in graph_of_users.keys:
-        followers = len(graph_of_users.get(user))
-        if followers > max_followers:
-            influencers = [user]
-            max_followers = followers
-        elif followers == max_followers:
-            influencers.append(user)
-    return influencers
-    
 
-def most_active(graph_of_users):
-    follower_counts = Dictionary()
-    for user_id, followers_id in graph_of_users.items():
-        for follower_id in followers_id:
-            if not follower_counts.contains(follower_id):
-                follower_counts.add(follower_id, 0)
-            follower_counts.add(follower_id, follower_counts.get(follower_id) + 1)
-    most_common_follower_id = max(follower_counts.keys, key=lambda x: follower_counts.get(x))
-    return most_common_follower_id
+class NetworkAnalysis:
+    def __init__(self, file_string):
+        # Initialize the graph of users using data from the database
+        users = DataBase.get_users_info(file_string)
+        self.graph_of_users = {}
+        for user in users:
+            self.graph_of_users[user.id] = user.followers
+        
+        #to test only
+        #self.graph_of_users={1: [1,6], 2: [2, 3,6], 3: [1,6],4:[1,2,3,5,6],5:[6],6:[]}
+            
+    def print_graph_of_users(self):
+        print(self.graph_of_users)
+        
+    def most_influencer(self):
+        # Find the users with the most followers
+        influencers = []
+        max_followers = 0
+        for user in self.graph_of_users.keys():
+            followers = len(self.graph_of_users[user])
+            if followers > max_followers:
+                influencers = [user]
+                max_followers = followers
+            elif followers == max_followers:
+                influencers.append(user)
+        return influencers
 
-def mutual_followers(user1, user2, graph_of_users):
-    followers1 = graph_of_users.get(user1)
-    followers2 = graph_of_users.get(user2)
-    mutual_followers = []
-    for follower in followers1:
-        if graph_of_users.contains(follower) and followers2.count(follower) > 0 and follower not in [user1, user2]:
-            mutual_followers.append(follower)
-    return mutual_followers
+    def most_active(self):
+        # Find the user who follows most users
+        following_counts = {}
+        for user_id, followers_id in self.graph_of_users.items():
+            for follower_id in followers_id:
+                if follower_id not in following_counts:
+                    following_counts[follower_id] = 0
+                following_counts[follower_id] += 1
+        most_active = max(following_counts, key=lambda x: following_counts[x])
+        return most_active
 
-def followers_of_followers(user_id, graph_of_users):
-    result = []
-    followers = graph_of_users.get(user_id)
-    for follower in followers:
-        follower_of_follower = graph_of_users.get(follower)
-        if follower_of_follower:
+
+    def mutual_followers(self, user1, user2):
+        try:
+            #Handle exception if a user not exist in the graph
+            followers1 = self.graph_of_users[user1]
+            followers2 = self.graph_of_users[user2]
+        
+        except KeyError:
+            print(f"One of the user IDs {user1} or {user2} is not in the graph of users.")
+            return []
+        
+        # Find the mutual followers of two users
+        mutual_followers = []
+        for follower in followers1:
+            if follower in self.graph_of_users and follower in followers2 and follower not in [user1, user2]:
+                mutual_followers.append(follower)
+        return mutual_followers
+
+
+    def followers_of_followers(self, user_id):
+        try:
+            #catch an exception is user index not in the graph
+            followers = self.graph_of_users[user_id]
+        except KeyError:
+            print(f"User with ID {user_id} not found in the graph")
+            return []
+        # Find the followers of followers of a user
+        result = []
+        for follower in followers:
+            follower_of_follower = self.graph_of_users.get(follower, [])
             for f in follower_of_follower:
                 if f not in result and f not in followers and f not in [user_id]:
                     result.append(f)
-    return result    
-
-
-
+        return result
+    
+    
 if __name__ == '__main__':
+    # file_string = read_file(r"path")
+    # network_analysis = NetworkAnalysis(file_string)
+    # network_analysis.print_graph_of_users()
 
-    file_string = read_file() #Add file here
+    # print(f" Most Influencer users are: {network_analysis.most_influencer()}")
+    # print(f" Most Active user is : {network_analysis.most_active()}")
+    # print(f" Mutual followers between {3} and {4} are : {network_analysis.mutual_followers(3, 4)}")
+    # print(f" Suggested users for user {7} : {network_analysis.followers_of_followers(7)}")
     
-    users = DataBase.get_users_info(file_string)
-
-    graph_of_users = Dictionary()
-    
-    for user in users:
-        graph_of_users.add(user.id, user.followers)
-    #for key in graph_of_users.keys:
-    #    print(f"User ID: {key} Followers ID: {graph_of_users.get(key)}")
-    
-    # print(f" Most Influencer users are: {most_influencer(graph_of_users)}")
-    
-    # print(f" Most Active user is : {most_active(graph_of_users)}")
-    
-    # print(f" Mutual followers between {1} and {2} are : {mutual_followers(1,2, graph_of_users)}")
-    
-    # print(f" Suggested users : {followers_of_followers(1, graph_of_users)}")
-
+    pass
